8dda0caee6b3e9caff3955bdc35adf8e25bbdc46
RBGMX77 <102673627+RBGMX77@users.noreply.github.com>
2022-03-30 16:15:00 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,17 +11,5 @@ module.exports = {
         permanent: true,
       },
     ]
-  },
-  env: {
-    ROOT_URL: 'http://localhost:3000'||'https://localhost:3000',
-    MAIL_USERNAME : 'romain.breuil@gmx.com',
-    MAIL_PASSWORD : 'Parissg772*'
-  },
-  theme: {
-    extend: {
-      backgroundImage: {
-       'pic': "url('/pic.jpg')",
-      }
-    }
   }
 }
